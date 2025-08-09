0ef96d548159d60554807c1355da360c3f801ddd
jiaHongShih <shih0010@algonquinlive.com>
2025-08-09 18:03:49 -0400

    fixing

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://jiaHongShih.github.io/gtg_pizzaWednesday/
-// base '/gtg_pizzaWednesday/'
 export default defineConfig({
   plugins: [react()],
-  base: '/gtg_pizzaWednesday/',
+  base: '/gtg_pizzaWednesday/',   
 })
